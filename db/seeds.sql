b01e0b2a8ff178442373c632c3aa7aff368962d3
unknown <vicduar@gmail.com>
2024-05-01 23:24:47 -0600

    minor changes

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,29 +3,29 @@ INSERT INTO apparition
     (year, location, witnesses)
 VALUES
     ('40','Zaragoza, Spain','St. James the Apostle'),
-    ('1061', 'Walsingham, England','Reicheldis de Faverches')
-    ('1251','Aylesford, England','St. Simon Stock')
-    ('1490','Monte Figogna, Italy','Benedetto Pareto')
-    ('1531','Mexico City, Mexico','St. Juan Diego, Juan Bernardino')
-    ('1570, 1597','Nadu, India','Tamil Krishnannesti Sankaranarayanan and an unknown boy')
-    ('1608','Siluva, Lithuania','Several townspeople')
-    ('1798','Quang Tri, Vietnam','Several townspeople')
-    ('1830','Paris, France','St Catherine Laboure')
-    ('1842','Rome, Italy','Marie-Alphonse Ratisbonne')
-    ('1846', 'La Salette, France','Melanie Mathieu-Calvat, Maximim Giraud')
-    ('1858','Lourdes, France','St. Bernadette Soubirous')
-    ('1859','Wisconsin, USA','Adele Brise')
-    ('1871','Pontmain, France','Eugene & Joseph Barbadette, Jeanne-Marie Lebosse, Francoise Richer')
-    ('1876','Pellovoisin, France','Estelle Faguette')
-    ('1877','Gietzwald, Poland','Stanislawa Samulowska, Justyna Szafrynska')
-    ('1879','Knock, Ireland','15 townspeople')
-    ('1900','Hebei, China','700 people')
+    ('1061', 'Walsingham, England','Reicheldis de Faverches'),
+    ('1251','Aylesford, England','St. Simon Stock'),
+    ('1490','Monte Figogna, Italy','Benedetto Pareto'),
+    ('1531','Mexico City, Mexico','St. Juan Diego, Juan Bernardino'),
+    ('1570, 1597','Nadu, India','Tamil Krishnannesti Sankaranarayanan and an unknown boy'),
+    ('1608','Siluva, Lithuania','Several townspeople'),
+    ('1798','Quang Tri, Vietnam','Several townspeople'),
+    ('1830','Paris, France','St Catherine Laboure'),
+    ('1842','Rome, Italy','Marie-Alphonse Ratisbonne'),
+    ('1846', 'La Salette, France','Melanie Mathieu-Calvat, Maximim Giraud'),
+    ('1858','Lourdes, France','St. Bernadette Soubirous'),
+    ('1859','Wisconsin, USA','Adele Brise'),
+    ('1871','Pontmain, France','Eugene & Joseph Barbadette, Jeanne-Marie Lebosse, Francoise Richer'),
+    ('1876','Pellovoisin, France','Estelle Faguette'),
+    ('1877','Gietzwald, Poland','Stanislawa Samulowska, Justyna Szafrynska'),
+    ('1879','Knock, Ireland','15 townspeople'),
+    ('1900','Hebei, China','700 people'),
     ('1917','Fatima, Portugal','Lucia Santos, Jacinta Marto, Francis Marto, plus >70,000 who witnessed the miracle of the sun')
     ('1932-1933','Beauraning, Belgium','Andreee Degeimbre, Gilgerte Degeimbre, Albert & Fernande & Gilberter Voisin','Banneux, Belgium')
     ('1933','Banneux, Belgium'.'Marietta Beco')
-    ('1945','New York, USA','Joseph Vitolo)
+    ('1945','New York, USA','Joseph Vitolo')
     ('1947',"L'lle-Bouchard, France",'Jacqueline & Jeanette Aubry, Nicole Robin, Laura Croizon, Tersita Castillo')
-    ('1948','Batangas, Philippines',Terisita Castillo)
+    ('1948','Batangas, Philippines','Terisita Castillo')
     ('1968','Cairo, Egypt','Estimated > 1,000,000 people, including President Abdel Nassar')
     ('1973','Akita, Japan','Sr. Agnes Katsuko Sasagawa')
     ('1980','San Francisco de Cuapa','Bernardo Martinez')
