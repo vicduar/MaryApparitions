63468ec251989ded23240bd09ab1c7ce3ec12ce4
unknown <vicduar@gmail.com>
2024-05-05 03:21:54 -0600

    added alts

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,6 @@ Create database mary;
 \c mary
 
 create Table apparition (
-    id serial primary key,
     year varchar(20) not null,
     location varchar(50) not null,
     witnesses varchar(100) not null
