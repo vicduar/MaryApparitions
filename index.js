911d12901c12471a3bcdb271c27ceb4e7955b340
unknown <vicduar@gmail.com>
2024-05-29 11:22:11 -0600

    responsive img

---
+++ index.js
@@ -0,0 +1,10 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const directory = './assets/img';
+
+fs.readdirSync(directory).forEach(file => {
+  sharp(`${directory}/${file}`)
+    .resize(200, 100) // width, height
+    .toFile(`${directory}/${file}-small.jpg`);
+  });
+  
